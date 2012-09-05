b65c2f36059b60f5c8bcbf527feefa22d19cd666
Alireza Farahani <ar_d_farahani@yahoo.com>
2012-09-05 20:54:08 +0430

    packs and sets which a team has been won added.

--- src/logic/Team.java
+++ src/logic/Team.java
@@ -1,11 +1,16 @@
 package logic;
 
 public class Team {
+	// Alireza: I'm not sure whether sets and packs should be put here or in table!
+	// @hadi: make sure new methods work correctly. you new my programming!
+	private Player[] players;
+	private int setsWon;
+	private Card[][] packsWon;
 	public Team(Player player1, Player player2){
 		players = new Player[]{player1, player2};
+		setsWon = 0;
+		packsWon = new Card[7][4];
 	}
-	
-	private Player[] players;
 
 	/**
 	 * @return the players
@@ -13,5 +18,16 @@ public class Team {
 	public Player[] getPlayers() {
 		return players;
 	}
-
+	
+	public void addSetsWon() {
+		setsWon++;
+	}
+	
+	public int getSetsWon() {
+		return setsWon;
+	}
+	
+	public void addPacksWon(Card[] pack){
+		packsWon[packsWon.length] = pack;
+	}
 }
