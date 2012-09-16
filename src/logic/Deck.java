a65703e395f46a48be78b5d6e78042e6a547ce9c
Hadi Zolfaghari <zerobit30@gmail.com>
2012-09-16 23:25:14 +0430

    yeee

--- src/logic/Deck.java
+++ src/logic/Deck.java
@@ -6,7 +6,6 @@ import java.util.Random;
  * A Deck of 52 cards, all cards in the game should be accessed from this deck
  */
 public class Deck {
-	
 	/* Bara inke harvagh ke too code lazem shod ke ye card ro begirim, nayaym hey hardafe
 	 * new konim ye card ke baas she az har card 1000 ta new she, too in class 52 ta card
 	 * ro yek bar new mikonim, bad hardafe khastim ye cardiro az getCard() e in migirim ono
